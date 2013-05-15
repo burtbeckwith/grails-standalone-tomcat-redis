b0fb2f4de10177f9805172c7805c50bde528428e
Burt Beckwith <burt@burtbeckwith.com>
2013-05-14 22:40:14 -0400

    fixed release script

--- release.sh
+++ release.sh
@@ -2,8 +2,8 @@ rm -rf target/release
 mkdir target/release
 cd target/release
 git clone git@github.com:burtbeckwith/grails-standalone-tomcat-redis.git
-cd standalone-tomcat-redis
+cd grails-standalone-tomcat-redis
 grails clean
 grails compile
-grails publish-plugin --noScm --snapshot --stacktrace
-#grails publish-plugin --noScm --stacktrace
+#grails publish-plugin --noScm --snapshot --stacktrace
+grails publish-plugin --noScm --stacktrace
