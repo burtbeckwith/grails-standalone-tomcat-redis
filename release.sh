c9d3962ec1201a7e125929b4358eb024cb449bb5
Burt Beckwith <burt@burtbeckwith.com>
2013-11-18 16:24:10 -0500

    cleanup

--- release.sh
+++ release.sh
@@ -5,5 +5,6 @@ git clone git@github.com:burtbeckwith/grails-standalone-tomcat-redis.git
 cd grails-standalone-tomcat-redis
 grails clean
 grails compile
-#grails publish-plugin --noScm --snapshot --stacktrace
-grails publish-plugin --noScm --stacktrace
+
+#grails publish-plugin --snapshot --stacktrace
+grails publish-plugin --stacktrace
